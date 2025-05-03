6344eec8f28ce882d485a891740404358bc2b21e
Aaryan Krishna <aaryan.krishna2024@nst.rishihood.edu.in>
2025-05-04 01:37:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import About from './Components/About/About';
+import Skills from './Components/Skills/Skills';
+import Projects from './Components/Projects/Projects';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Hero />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Hero.css"
+import profile_img from '../../assets/profile.png'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <img src={profile_img} alt=""/>
+        <h1><span>I'm Aaryan Krishna</span>, frontend developer and video editor.</h1>
+        <p>I am an aspiring full stack developer. Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+        <div className="hero-action">
+            <div className="hero-connect">Connect with Me</div>
+            <div className="hero-projects">View Projects</div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
