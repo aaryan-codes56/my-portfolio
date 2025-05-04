dd660c2bbd41d7c935bf4cb9d52374e090227fdb
Aaryan Krishna <aaryan.krishna2024@nst.rishihood.edu.in>
2025-05-05 03:10:12 +0530

    again

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
         <img src={profile_img} alt="" className='pro-img' />
-        <h1><span>I'm Aaryan Krishna</span>, Frontend Developer and Graphic Designer.</h1>
-        <p>I’m a Frontend Developer on my journey to becoming a Full Stack developer. I also work as a video editor and graphic designer, creating visually engaging and meaningful digital content.</p>
+        <h1><span>I'm Aaryan Krishna,</span> Frontend Developer and Graphic Designer.</h1>
+        <p>I'm a developer working towards becoming a Full Stack Developer, while also specialising in video editing and graphic design to create visually compelling and impactful digital content.</p>
         <div className="hero-action">
             <div className="hero-connect">
               <a className='anchor-link1' href="#contact">Get in touch</a>
